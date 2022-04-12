5bfff2fd4d6767d1878a2886d90da122c5ded08c
YukiShinji <lex.lipov@mail.ru>
2022-04-12 23:56:31 +0300

    First task end

--- HomeworkC#1/Program.cs
+++ HomeworkC#1/Program.cs
@@ -1,4 +1,6 @@
-﻿/*Console.Write("Input first number ");
+﻿// Task 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+/*Console.Write("Input first number ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input second number ");
 int num2 = Convert.ToInt32(Console.ReadLine());
