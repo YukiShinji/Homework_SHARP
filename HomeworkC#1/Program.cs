769d302e8d0f77eab6a5576480fe7619ac83bfd5
YukiShinji <lex.lipov@mail.ru>
2022-04-12 23:54:54 +0300

    First task end

--- HomeworkC#1/Program.cs
+++ HomeworkC#1/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿/*Console.Write("Input first number ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) {
+    Console.Write(num1 + " Bigger then " + num2);
+}   else {
+    Console.Write(num2 + " Bigger then " + num1);
+}
+*/
