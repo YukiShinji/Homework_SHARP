6bacf02ed482498c51181abcecddab4df2cbbc28
YukiShinji <lex.lipov@mail.ru>
2022-04-26 21:40:11 +0300

    All task's is done

--- HomeworkC#4/Program.cs
+++ HomeworkC#4/Program.cs
@@ -1,4 +1,5 @@
-﻿int[] CreatRandomArray(int size, int minVal, int maxVal) {
+﻿
+int[] CreatRandomArray(int size, int minVal, int maxVal) {
     int[] array = new int[size];
     Console.Write("Your massive: ");
     for (int i = 0; i < size; i++)
@@ -63,4 +64,33 @@ int userMaxValue = Convert.ToInt32(Console.ReadLine());
 int[] userRandomMassive = CreatRandomArray(userSize, userMinValue, userMaxValue);
 int summOddNumbers = sumOfOddNumbersInTheArray(userRandomMassive);
 Console.WriteLine("This is a summ odd numbers: " + summOddNumbers);
+*/
+
+// Task 3: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+/*
+void differenceBetweenTheMaximumAndMinimumArrayElements (int[] userArray, int userMinValue, int userMaxValue) {
+    int between = 0;
+    int maxVal = userMinValue;
+    int minVal = userMaxValue;
+
+    for (int i = 0; i < userArray.Length; i ++) {
+        if (userArray[i] > maxVal) {
+            maxVal = userArray[i];
+        }   else if (userArray[i] < minVal) {
+            minVal = userArray[i];
+        }
+    }
+    Console.Write("This is your's between: " + (between = maxVal-minVal));
+
+}
+Console.WriteLine("Insert size, min and max value in massive ");
+Console.Write("Insert size ");
+int userSize = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert min value ");
+int userMinValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert max value ");
+int userMaxValue = Convert.ToInt32(Console.ReadLine());
+
+int[] userRandomMassive = CreatRandomArray(userSize, userMinValue, userMaxValue);
+differenceBetweenTheMaximumAndMinimumArrayElements(userRandomMassive, userMinValue, userMaxValue);
 */
