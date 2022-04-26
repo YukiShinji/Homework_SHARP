e389e22bde47371909e140978582a18a2623c924
YukiShinji <lex.lipov@mail.ru>
2022-04-26 20:53:38 +0300

    First task is done

--- HomeworkC#4/Program.cs
+++ HomeworkC#4/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿int[] CreatRandomArray(int size, int minVal, int maxVal) {
+    int[] array = new int[size];
+    Console.Write("Your massive: ");
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minVal, maxVal);
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine(" ");
+    return array;
+    
+}
+
+// Task 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+void showEvenNumbers (int[] userArray) {
+    int arrayCount = 0;
+    for (int i = 0; i < userArray.Length; i ++) {
+        if (userArray[i] % 2 == 0) {
+            arrayCount ++;
+        }
+    }
+    if (arrayCount == 0) {
+        Console.WriteLine("There are no even numbers in this array ");
+    }   else {
+        Console.WriteLine("There are " + arrayCount + " even numbers in this array ");
+    }
+}
+
+Console.WriteLine("Insert size, min and max value in massive ");
+Console.Write("Insert size ");
+int userSize = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert min value ");
+int userMinValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert max value ");
+int userMaxValue = Convert.ToInt32(Console.ReadLine());
+
+int[] userRandomMassive = CreatRandomArray(userSize, userMinValue, userMaxValue);
+showEvenNumbers(userRandomMassive);
