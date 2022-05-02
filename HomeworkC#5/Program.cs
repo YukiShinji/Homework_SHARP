32933553c65e2217936132da73d738ab0ce06e6a
YukiShinji <lex.lipov@mail.ru>
2022-05-02 14:36:40 +0300

    Initial first Task HomeworkC#5

---
+++ HomeworkC#5/Program.cs
@@ -0,0 +1,18 @@
+﻿// Task 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*
+void returnOfNatural (int userNameA, int userNameB) {
+    int newReturnNumber = 1;
+    for (int i = 0; i < userNameB; i++) {
+        newReturnNumber = newReturnNumber * userNameA;
+    }
+    Console.WriteLine("Your number is: " + newReturnNumber);
+}
+
+Console.WriteLine("Insert two number: ");
+Console.Write("Insert first number: ");
+int usernumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert second number: ");
+int usernumberB = Convert.ToInt32(Console.ReadLine());
+
+returnOfNatural(usernumberA, usernumberB);
+*/
