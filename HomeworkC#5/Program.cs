dc1c09e8980655330cfa7f314cee13aae10e4107
YukiShinji <lex.lipov@mail.ru>
2022-05-02 15:09:42 +0300

    All homework is done

--- HomeworkC#5/Program.cs
+++ HomeworkC#5/Program.cs
@@ -35,3 +35,29 @@ int userNumber = Convert.ToInt32(Console.ReadLine());
 int finalNumber = sumOfDigits(userNumber);
 Console.WriteLine("This is the summ: " + finalNumber);
 */
+
+// Task 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+/*
+int[] CreatRandomArray(int size, int minVal, int maxVal) {
+    int[] array = new int[size];
+    Console.Write("Your massive: ");
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minVal, maxVal);
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine(" ");
+    return array;
+    
+}
+
+Console.WriteLine("Insert size, min and max value in massive ");
+Console.Write("Insert size ");
+int userSize = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert min value ");
+int userMinValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert max value ");
+int userMaxValue = Convert.ToInt32(Console.ReadLine());
+
+CreatRandomArray(userSize, userMinValue, userMaxValue);
+*/
