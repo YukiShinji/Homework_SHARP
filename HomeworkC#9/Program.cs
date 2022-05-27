cb02c209513916184534ea854d2beee0fd6cfe1d
YukiShinji <lex.lipov@mail.ru>
2022-05-27 19:39:41 +0300

    HomeworkC#9 is fixed

--- HomeworkC#9/Program.cs
+++ HomeworkC#9/Program.cs
@@ -1,10 +1,12 @@
 ﻿// Task 1: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 /*
-void findSummBeetwenNumber(int userFirstNumber, int userSecondNumber) {
-    int countNumbers = 0;
-    for (int i = userFirstNumber; i <= userSecondNumber; i++)
-        countNumbers += i;
-    Console.WriteLine(countNumbers);
+int findSummBeetwenNumber(int usernum1, int usernum2){
+    int result = usernum1;
+    for (int i = ++usernum1; i <= usernum2; i++){
+        result += i;
+        findSummBeetwenNumber(++usernum1, usernum2);
+    }
+    return result;
 }
 
 Console.Write("Insert number first: ");
@@ -12,24 +14,22 @@ int userFirstNumber = Convert.ToInt16(Console.ReadLine());
 Console.Write("Insert number second: ");
 int userSecondNumber = Convert.ToInt16(Console.ReadLine());
 
-findSummBeetwenNumber(userFirstNumber, userSecondNumber);
+int finalSumm = findSummBeetwenNumber(userFirstNumber, userSecondNumber);
+Console.WriteLine("Summ is: " + finalSumm);
 */
 
 // Task 2: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
 /*
-void summInUserNumber (int userNumber) {
-    int summ = 0;
-    int delen = 0;
-
-    while(userNumber > 0) {
-        delen = userNumber % 10;
-        summ = summ + delen;
-        userNumber = userNumber / 10;
-    }
-    Console.WriteLine("Summ number is: " + summ);
+int summInUserNumber(int userNumber1) {
+    if (userNumber1 / 10 != 0) {
+        return userNumber1 % 10 + summInUserNumber(userNumber1 / 10); 
+    }   else {
+            return userNumber1 % 10;
+        }
 }
 
-Console.WriteLine("Insert your number: ");
+Console.Write("Insert your number: ");
 int userNumber1 = Convert.ToInt32(Console.ReadLine());
-summInUserNumber(userNumber1);
+int finalNumber = summInUserNumber(userNumber1);
+Console.WriteLine("Summ numbers in number: " + finalNumber);
 */
